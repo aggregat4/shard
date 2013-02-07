bca57333c51c627a7b192ba956749de20df9d8dd
Boris Terzic <boris@terzic.be>
2013-02-07 22:29:52 +0100

    have a possibly workable abstraction for routing, removing earlier attempt

--- shard2/src/main/scala/a4/shard/RequestHandler.scala
+++
@@ -1,7 +0,0 @@
-package a4.shard
-
-trait RequestHandler {
-  
-  def matches(path: String) : Boolean
-  
-}
--- shard2/src/main/scala/a4/shard/ShardWikiServlet.scala
+++ shard2/src/main/scala/a4/shard/ShardWikiServlet.scala
@@ -6,12 +6,7 @@ import javax.servlet.http.HttpServletResponse
 
 class ShardWikiServlet extends HttpServlet {
 
-  val router= Router(List())
-   
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) : Unit = {
-    
-    
-    req.getParts()
     val os = resp.getOutputStream()
     os.print("Hello world")
     os.close()
