2c985f97d588bb406e2dfe209d7c0b49a3df8e41
Boris Terzic <boris@terzic.be>
2013-06-25 22:31:15 +0200

    This seems to be about as succint as it gets. Note that I have extracted the content method as well from Page since now that there is an actual File this can be handled externally

--- shard2/src/main/scala/a4/shard/Domain.scala
+++ shard2/src/main/scala/a4/shard/Domain.scala
@@ -6,11 +6,6 @@ import a4.util.PathUtil
 import a4.shard.transforming.PageContentTransformer
 import a4.shard.templating.PageRenderer
 
-/**
- * TODO: 
- * Refactoring domain model to be purely structural: what is the tree like, what content does it have, what metadata
- * Rendering is completely outside of this. Cover this with unit tests
- */
 
 case class Wiki(val id: String, val name: String, val location: String) {
   def urlPath : String = "/wiki/" + id
@@ -22,97 +17,46 @@ trait Content {
   def relativeUrl : String
   def name : String
   def file : File
-  def exists : Boolean // don't like this here
   def parent : Content
+  def exists : Boolean
 }
 
 case class Root(val wiki: Wiki) extends Content {
   override def relativeUrl : String = ""
   override def name : String = ""
   override def file : File = new File(wiki.location)
+  override def parent : Content = this // not sure about this, seems too clever, what else though? if I return Option then I need special casing in subclasses
+  override def exists : Boolean = true
+}
+
+case class Folder(val wiki: Wiki, val name: String, val parent: Folder) extends Content {
+  override def file : File = new File(parent.file, name)
+  override def relativeUrl : String = parent.relativeUrl + name + "/"
   override def exists : Boolean = file.exists && file.isDirectory
-  override def parent : Content = this // not sure about this, seems to clever, what else though? if I return Option then I need special casing in subclasses
+
+  private def filterFiles[T <: Content](ctor: File => T) : List[T] = file.listFiles.map(ctor).filter(_.exists).toList
+    
+  def pages : List[Page] = filterFiles[Page](f => new Page(wiki, f.getName, this)) 
+  def attachments : List[Attachment] = filterFiles[Attachment](f => new Attachment(wiki, f.getName, this))  
+  def folders : List[Folder] = filterFiles[Folder](f => new Folder(wiki, f.getName, this)) 
 }
 
-case class Folder(val wiki: Wiki, val name: String, val parent: Option[Folder]) extends Content {
-  override def file : File = parent match {
-    case Some(folder) => new File(folder.file, name)
-    case None => new File(wiki.location)
-  }
-  override def relativeUrl : String = parent match {
-    case Some(folder) => folder.relativeUrl
-    case None => wiki.urlPath
-  }
-  override def exists : Boolean = file.exists && file.isDirectory
+case class Page(val wiki: Wiki, val name: String, val parent: Folder) extends Content {
+  private val PAGE_SUFFIX : String = ".shard.md"
+  private val shortName : String = if (name.endsWith(PAGE_SUFFIX)) name.substring(0, name.length - PAGE_SUFFIX.length) else name
   
-  def pages : List[Page] = 
-    file
-  		.listFiles
-  		.filter(f => f.isFile && f.getName.endsWith(".shard.md"))
-  		.map(f => new Page(wiki, PathUtil.toConcretePath(folderUrl, f.getName.substring(0, f.getName.length - ".shard.md".length))))
-  		.toList
-  	
-//  def files : List[WikiFile] = List()
-  		
-  		
-// I need a Page type for attachments
-//    folderFile.listFiles
-//  	.filter(f => f.isFile && ! f.getName.endsWith(".shard.md"))
-//  	.map(f => Addressable(f.getName))
-  
-  def folders : List[Folder] = 
-    (folderFile
-    	.listFiles
-    	.filter(f => f.isDirectory())
-    	.map(f => new Folder(wiki, PathUtil.toFolderPath(folderUrl, f.getName))))
-    .toList
-}
-
-case class Page(val wiki: Wiki, val name: String, val parent: Option[Folder]) extends Content {
-  private val pageSuffix = ".shard.md"
-  private val file = new File(wiki.location, pageUrl + pageSuffix)
-  
-  override def relativeUrl : String = wiki.urlPath + pageUrl
-  override def name : String = PathUtil.name(pageUrl) 
-  override def exists : Boolean = pageFile.exists && pageFile.isFile
-  override def parent : Option[Folder] = Some(new Folder(wiki, PathUtil.parent(pageUrl)))
-
-  def content : String = FileUtil.readAsUtf8(pageFile)
-}
-
+  override def file : File = new File(parent.file, name)
+  override def relativeUrl : String = parent.relativeUrl + shortName
 //
-//case class Attachment extends Content {
-//  // TODO
-//}
-
-//case class ConcretePage(val wiki: Wiki, val page: String, val contentTransformer: PageContentTransformer) extends WikiPage {
-//  private val template = "page.mustache"
-//   
-//  def render(pageRenderer: PageRenderer) = 
-//    pageRenderer.render(template, Map(
-//      "pageTitle" -> wiki.name, 
-//      "wikiPage" -> this, 
-//      "pageContent" -> contentTransformer.transform(this, content),
-//      "context" -> this.parent
-//    ))
-//}
-
-
-//case class FolderPage(val wiki: Wiki, val folder: String, val contentTransformer: PageContentTransformer) extends WikiPage {
-//  private val template = "folder.mustache"
-//   
-//  	
-//  def render(pageRenderer: PageRenderer) =
-//    pageRenderer.render(template, Map(
-//      "pageTitle" -> wiki.name, 
-//      "wikiPage" -> this, 
-//      "pages" -> pages,
-//      "files" -> files,
-//      "folders" -> folders))
-//  }
-
-
-case class RootPage(val wikis: List[Wiki]) {
-  private val template = "root.mustache"
-  def render(pageRenderer: PageRenderer) = pageRenderer.render(template, Map("pageTitle" -> "Shard", "wikis" -> wikis))
+//  def content : String = FileUtil.readAsUtf8(file)
 }
+
+case class Attachment(val wiki: Wiki, val name: String, val parent: Folder) extends Content {
+  override def file = new File(parent.file, name)
+  override def relativeUrl : String = parent.relativeUrl + name
+}
+
+//case class RootPage(val wikis: List[Wiki]) {
+//  private val template = "root.mustache"
+//  def render(pageRenderer: PageRenderer) = pageRenderer.render(template, Map("pageTitle" -> "Shard", "wikis" -> wikis))
+//}
