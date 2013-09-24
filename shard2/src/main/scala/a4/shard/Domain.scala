e8c7612ef0872b0ca44ad09e5e4de9f2c209a791
Boris Terzic <boris@terzic.be>
2013-09-24 22:21:09 +0200

    f inally having a compiling domain, let's try to work with this

--- shard2/src/main/scala/a4/shard/Domain.scala
+++ shard2/src/main/scala/a4/shard/Domain.scala
@@ -10,43 +10,45 @@ case class Wiki(val id: String, val name: String, val location: File) {
   def urlPath : String = "/wiki/" + id
 }
 
-/**
- * TODO New idea: have a ContentHypothesis class taking a wiki and a relative path. 
- * It has a method test() that returns Either a Content or an Error (look Either up).
- * This class has the logic for mapping a relative URL to an actual object. The Content 
- * subclasses then become simpler since they would take a wiki and a 
- * File object as input and would assume that they are valid.
- */
-
 object Content {
-  def isRoot(name: String) = PathUtil.isEmpty(name.trim) || name.trim == "/"
-  def toContent(wiki: Wiki, name: String) : Content = {
-    if (isRoot(name)) Folder(wiki)
-  }
+  private def isRoot(name: String) = PathUtil.isEmpty(name.trim) || name.trim == "/"
+  
+  def validFile(file: File) : Boolean = file.exists && file.isFile 
+  def validDirectory(file: File) : Boolean = file.exists && file.isDirectory
+  def isPage(file: File) : Boolean = validFile(file) && file.getName.endsWith(Page.PAGE_SUFFIX)
+  def isAttachment(file: File) : Boolean = validFile(file) && ! isPage(file)
+  def isFolder(file: File) : Boolean = ! isPage(file) && ! isAttachment(file) && validDirectory(file) 
+   
+  def determineType(wiki: Wiki, file: File) : Option[Content] = 
+    if (isPage(file)) Some(Page(wiki, file))
+  	else if (isAttachment(file)) Some(Attachment(wiki, file))
+  	else if (isFolder(file)) Some(Folder(wiki, file))
+  	else None
+  
+  def toContent(wiki: Wiki, name: String) : Option[Content] =
+    if (isRoot(name)) Some(Folder(wiki, wiki.location))
+    else determineType(wiki, new File(wiki.location, name + Page.PAGE_SUFFIX)) match {
+      case Some(c) => Some(c)
+      case None => determineType(wiki, new File(wiki.location, name)) 
+    }
 }
 
 trait Content {
   def wiki : Wiki
+  def file : File
   def parent : Content
   def relativeUrl : String
-  def name : String
-  def file : File
-//  def exists : Boolean  
+  def exists : Boolean  
 }
 
-//case class Root(val wiki: Wiki) extends Content {
-//  override def relativeUrl : String = ""
-//  override def name : String = ""
-//  override def file : File = wiki.location
-//  override def parent : Content = this // not sure about this, seems too clever, what else though? if I return Option then I need special casing in subclasses
-//  override def exists : Boolean = true
-//}
-
 case class Folder(val wiki: Wiki, val file: File) extends Content {
-  override def parent : Content = if (file.equals(wiki.location)) this else Folder(wiki, file.getParentFile()) 
-  override def relativeUrl : String = name + "/"
-//  override def exists : Boolean = file.exists && file.isDirectory
+  private def isRoot : Boolean = file.equals(wiki.location)
   
+  override def parent : Content = if (isRoot) this else Folder(wiki, file.getParentFile()) 
+  override def relativeUrl : String = file.getName + "/"
+  override def exists : Boolean = file.exists && file.isDirectory
+  
+  // TODO possibly reconsider whether I want this logic inside of the Folder class and not outside such as the construction logic in Content
   private def filterFiles[T <: Content](ctor: File => T) : List[T] = file.listFiles.map(ctor).filter(_.exists).toList
     
   def pages : List[Page] 			 = filterFiles[Page](f => new Page(wiki, f)) 
@@ -54,27 +56,21 @@ case class Folder(val wiki: Wiki, val file: File) extends Content {
   def folders : List[Folder] 		 = filterFiles[Folder](f => new Folder(wiki, f)) 
 }
 
-case class Page(val wiki: Wiki, val name: String) extends Content {
-  private val PAGE_SUFFIX : String = ".shard.md"
-  private val shortName : String = if (name.endsWith(PAGE_SUFFIX)) name.substring(0, name.length - PAGE_SUFFIX.length) else name
-  private val fullName : String = if (name.endsWith(PAGE_SUFFIX)) name else name + PAGE_SUFFIX
+object Page {
+  val PAGE_SUFFIX : String = ".shard.md"
+}
+
+case class Page(val wiki: Wiki, val file: File) extends Content {  
+  private val name = file.getName 
+  private val shortName : String = if (name.endsWith(Page.PAGE_SUFFIX)) name.substring(0, name.length - Page.PAGE_SUFFIX.length) else name
   
-  override def parent : Content = Folder(wiki, PathUtil.parent(name))
+  override def parent : Content = Folder(wiki, file.getParentFile())
   override def relativeUrl : String = shortName
-  override def file : File = if (isRoot) wiki.location else new File(wiki.location, fullName)
-//  override def exists : Boolean = file.exists && file.isFile 
-//
-//  def content : String = FileUtil.readAsUtf8(file)
+  override def exists : Boolean = file.exists && file.isFile 
 }
 
-case class Attachment(val wiki: Wiki, val name: String) extends Content {
-  override def parent : Content = Folder(wiki, PathUtil.parent(name))
-  override def relativeUrl : String = name
-  override def file : File = if (isRoot) wiki.location else new File(wiki.location, name)
-//  override def exists : Boolean = file.exists && file.isFile 
+case class Attachment(val wiki: Wiki, val file: File) extends Content {
+  override def parent : Content = Folder(wiki, file.getParentFile)
+  override def relativeUrl : String = file.getName
+  override def exists : Boolean = file.exists && file.isFile 
 }
-
-//case class RootPage(val wikis: List[Wiki]) {
-//  private val template = "root.mustache"
-//  def render(pageRenderer: PageRenderer) = pageRenderer.render(template, Map("pageTitle" -> "Shard", "wikis" -> wikis))
-//}
